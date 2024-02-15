5064f9fa2bda6acc9f5f62f039c5a78aafd15b64
yahu1031 <hi2minnu@gmail.com>
2024-02-15 09:22:33 +0530

    Feat: Added yaml docs support

--- tools/serverpod_vscode_extension/src/extension.ts
+++ tools/serverpod_vscode_extension/src/extension.ts
@@ -1,19 +1,75 @@
 
-import { ExtensionContext, window } from 'vscode';
+import { execSync } from 'child_process';
+import { coerce, satisfies } from 'semver';
+import { ExtensionContext, Hover, languages, window } from 'vscode';
 import {
+	DocumentSelector,
 	LanguageClient,
 	LanguageClientOptions,
 	RevealOutputChannelOn,
 	ServerOptions,
-	TransportKind,
+	TransportKind
 } from 'vscode-languageclient/node';
-import { execSync } from 'child_process';
-import { satisfies, coerce } from 'semver';
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
 	let output;
+	const ds: DocumentSelector = [{ language: 'yaml'}, { language: 'Spy'}, { language: 'Serverpod yaml'}];
+	languages.registerHoverProvider(ds, {
+        provideHover(document, position, token) {
+            const range = document.getWordRangeAtPosition(position);
+			const word = document.getText(range);
+			const isKey = /^\s*([^#]*?):/.test(word);
+			/* 
+			 * get the line and remove part till the word
+			 * `name: String relation(name = usr_address, field = addressId)`
+			 *  -> ` String relation(name = usr_address, field = addressId)` 
+			 *  -> ` String` -> `String`
+			 */
+			const dataType = document.lineAt(position.line).text.split(word)[1].trim().split(' ')[0].trim();
+            if (!range || word.startsWith('#') || !isKey) {
+                return undefined;
+            }
+
+            // Extract the word
+            // const isValue = !isKey && /^\s*[^#:]+:\s*(.+)?$/.test(word);
+			// console.log(`---------- ${word} is Key: ${isKey} ----------`);
+			// console.log(`---------- ${word} is Value: ${isValue} ----------`);
+            // Get the line number of the current position
+            const lineNumber = position.line;
+
+            // Extract the comment right above the word
+            let comment = '';
+            for (let i = lineNumber - 1; i >= 0; i--) {
+                const line = document.lineAt(i).text.trim();
+				if (line === '') {
+					// Stop if an empty line is encountered
+					break;
+				}
+				if (line.startsWith('#')) { 
+					comment = comment.substring(1).trim();
+				}
+				// if any line does not start with a #, don't consider it a comment
+				if (!line.startsWith('#')) {
+					break;
+				}
+                comment = line + '\n' + comment;
+			}
+			// substring to remove the first character, which is a #
+			// also check if the first character is a #, otherwise we have a comment on the same line as the word
+			if (comment.charAt(0) === '#')
+			{
+				comment = comment.substring(1).trim();
+			}
+			if (dataType !== undefined || dataType !== '') {
+				const nullableDataType = dataType.endsWith('?') ? `${dataType.slice(0, -1)} || Null` : dataType;
+				comment = comment + '\n' + `\`\`\`dart\n${nullableDataType}\n\`\`\``;
+			}
+
+			return new Hover(comment);
+        }
+    });
 	try {
 		output = execSync('serverpod version');
 	} catch (e) {
